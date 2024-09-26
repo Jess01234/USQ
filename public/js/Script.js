cdaeff39bdd1f75288446fbebee4b7768557bce1
Jess01234 <162223551+Jess01234@users.noreply.github.com>
2024-09-26 11:04:23 -0600

    Resolviendo conflictos

---
+++ public/js/Script.js
@@ -0,0 +1,15 @@
+
+
+function HideWindow(){
+
+    var PopUp = document.getElementById("PopUp");
+
+    PopUp.style.display = 'none';
+};
+
+function ShowWindow(){
+
+    var PopUp = document.getElementById("PopUp");
+
+    PopUp.style.display = 'flex';
+};

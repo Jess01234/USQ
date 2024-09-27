34dab3a5ca56fddd960ecb4384891758ba24efcf
Jess01234 <162223551+Jess01234@users.noreply.github.com>
2024-09-27 10:54:27 -0600

    App

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/main', function(req, res, next){
+    res.render('main');
+});
+
+router.post('/main', function(req, res, next){
+    res.render('main');
+});
+
+module.exports = router;
